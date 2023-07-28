8481e8939addd962e0eef809ca14bcda84adc933
andrewmoy3 <andrewmoy3@gmail.com>
2023-07-28 17:42:54 -0400

    banner

---
+++ src/Components/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import '../css/App.css'
+import Banner from './Banner'
+
+function App() {
+
+  return (
+    <>
+      <Banner />
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Banner.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import '../css/Banner.css'
+
+export default function Banner(){
+    return (
+        <div className="banner" id="banner">
+            <div id="signature">Made by Andrew Moy</div>
+            <div id="title">Battleship</div>
+        </div>
+    )
+}
