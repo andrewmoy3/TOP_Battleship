6e3eb71fb6e4581b9ba5c830d9addf48b521a094
andrewmoy3 <andrewmoy3@gmail.com>
2023-07-31 16:21:17 -0400

    selecting squares on field

--- src/Components/Battlefield.jsx
+++ src/Components/Battlefield.jsx
@@ -1,32 +1,36 @@
-import React from "react";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import '../css/Battlefield.css';
 import Ship from "./Ship";
 
-export default function Battlefield( { Gameboard } ){
+export default function Battlefield({ Gameboard }) {
+  const [board, setBoard] = useState(Gameboard.getBoard());
 
-    const [board, setBoard] = useState(Gameboard.getBoard())
-    
-    useEffect(() => {
-        setBoard(Gameboard.getBoard());
-    }, [Gameboard]);
+  useEffect(() => {
+    const newBoard = [...Gameboard.getBoard()];
+    setBoard(newBoard);
+  }, [Gameboard]);
 
-    const place = function(x, y, dir){
-        Gameboard.placeShip(Ship(2), x, y, dir)
-        setBoard(Gameboard.getBoard())
-    }
+  const place = function (x, y, dir) {
+    const newBoard = [...board];
+    Gameboard.placeShip(Ship(2), x, y, dir);
+    setBoard(newBoard);
+  };
 
-    return (
-        <div className="battlefield">
-            {board.map((row, rowIndex) => (
-                <div key={rowIndex} className="battlefieldCol">
-                    {row.map((ship, columnIndex) => (
-                        <div 
-                        onClick={() => place(rowIndex, columnIndex, 'x')}
-                        className={ship ? 'box ship' : 'box'} key={columnIndex}>{ship.name}</div>
-                    ))}
-                </div>
-            ))}
-      </div>
-    )
+  return (
+    <div className="battlefield">
+      {board.map((row, rowIndex) => (
+        <div key={rowIndex} className="battlefieldCol">
+          {row.map((ship, columnIndex) => (
+            <div
+              onClick={() => place(rowIndex, columnIndex, 'x')}
+              className={ship ? 'box ship' : 'box'}
+              key={columnIndex}
+            >
+              
+            </div>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
 }
