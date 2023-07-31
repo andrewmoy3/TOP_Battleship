b9b57810e6460a5aa0d4c6ae77baccada7a90ba3
andrewmoy3 <andrewmoy3@gmail.com>
2023-07-31 10:56:09 -0400

    gameboard tests: placing ships

--- src/Components/Gameboard.js
+++ src/Components/Gameboard.js
@@ -3,13 +3,39 @@ import Ship from "./Ship"
 
 export default function Gameboard(){
 
-    const receiveAttack = function(){}
-    const placeShip = function(){}
-    const hasShipAt = function(){}
+    const size = 10;
+    const board = Array.from({ length: size }, () => Array.from({ length: size }, () => 0));
+
+    const receiveAttack = function(x, y){
+        if(board[x,y])return true;
+        return false;
+    }
+    const placeShip = function(ship, x, y, dir){
+        const len = ship.getLength();
+        for(let i=0;i<len;i++){
+            if(dir === 'x'){
+                if(board[x+i][y])return null;
+                board[x+i][y]++;
+            }else{
+                if(board[x][y+i])return null;
+                board[x][y+i]++;
+            }
+            if(x+i == size-1 && dir == 'x') x = - i - 1;
+            if(y+i == size-1 && dir == 'y') y = - i - 1;
+        }
+    }
+
+    const hasShipAt = function(x, y){
+        if(board[x][y])return true;
+        return false;
+    }
+
+    const getSize = function(){return size}
 
     return {
         receiveAttack,
         placeShip,
         hasShipAt,
+        getSize,
     }
 }
--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -6,6 +6,7 @@
 // Gameboards should be able to report whether or not all of their ships have been sunk.
 
 import Gameboard from "../Components/Gameboard";
+import Ship from '../Components/Ship'
 
 describe('Gameboard: placing ships', () => {
     
@@ -13,17 +14,90 @@ describe('Gameboard: placing ships', () => {
         const gameboard = Gameboard();
         const ship = Ship(3); // Create a ship with length 3
       
-        gameboard.placeShip(ship, 0, 0); // Place the ship at (0, 0)
+        gameboard.placeShip(ship, 0, 0, 'y'); // Place the ship at (0, 0)
       
-        // Check if the ship exists at the specified coordinates
         expect(gameboard.hasShipAt(0, 0)).toBe(true);
         expect(gameboard.hasShipAt(0, 1)).toBe(true);
         expect(gameboard.hasShipAt(0, 2)).toBe(true);
       
-        // Check other positions to ensure no ship exists there
         expect(gameboard.hasShipAt(1, 0)).toBe(false);
         expect(gameboard.hasShipAt(1, 1)).toBe(false);
-        // ... (continue checking other positions)
-      });      
+      });  
+      
+    it('can wrap around x coordinates', () => {
+        const gameboard = Gameboard();
+        const size = gameboard.getSize()-1;
+        const ship = Ship(3);
+        gameboard.placeShip(ship, size, 0,'x');
+        expect(gameboard.hasShipAt(size, 0)).toBe(true);
+        expect(gameboard.hasShipAt(0, 0)).toBe(true);
+        expect(gameboard.hasShipAt(1, 0)).toBe(true);
+        expect(gameboard.hasShipAt(2, 0)).toBe(false);
+        expect(gameboard.hasShipAt(size, 1)).toBe(false);
+        expect(gameboard.hasShipAt(0, size)).toBe(false);
+    })
 
+    it('can wrap around y coordinates', () => {
+        const gameboard = Gameboard();
+        const size = gameboard.getSize()-1;
+        const ship = Ship(3);
+        gameboard.placeShip(ship, 0, size, 'y');
+        expect(gameboard.hasShipAt(0, size)).toBe(true);
+        expect(gameboard.hasShipAt(0, 0)).toBe(true);
+        expect(gameboard.hasShipAt(0, 1)).toBe(true);
+
+        expect(gameboard.hasShipAt(0, 2)).toBe(false);
+        expect(gameboard.hasShipAt(1,size)).toBe(false);
+    })
+
+    it('can wrap around x coordinates', () => {
+        const gameboard = Gameboard();
+        const size = gameboard.getSize()-1;
+        const ship = Ship(3);
+        gameboard.placeShip(ship, size - 1, 0,'x');
+        expect(gameboard.hasShipAt(size - 1, 0)).toBe(true);
+        expect(gameboard.hasShipAt(size, 0)).toBe(true);
+        expect(gameboard.hasShipAt(0, 0)).toBe(true);
+        expect(gameboard.hasShipAt(1, 0)).toBe(false);
+        expect(gameboard.hasShipAt(size, 1)).toBe(false);
+        expect(gameboard.hasShipAt(0, size)).toBe(false);
+    })
+
+    it('can wrap around y coordinates', () => {
+        const gameboard = Gameboard();
+        const size = gameboard.getSize()-1;
+        const ship = Ship(3);
+        gameboard.placeShip(ship, 0, size - 1,'y');
+        expect(gameboard.hasShipAt(0, size - 1)).toBe(true);
+        expect(gameboard.hasShipAt(0, size)).toBe(true);
+        expect(gameboard.hasShipAt(0, 0)).toBe(true);
+        expect(gameboard.hasShipAt(0, 1)).toBe(false);
+        expect(gameboard.hasShipAt(1, size)).toBe(false);
+        expect(gameboard.hasShipAt(size, 0)).toBe(false);
+    })
+
+    it('does not allow two ships to occupy the same spot', () => {
+        const gameboard = Gameboard();
+        const ship1 = Ship(3);
+        const ship2 = Ship(5);
+        gameboard.placeShip(ship1, 2, 0,'y');
+        expect(gameboard.placeShip(ship2, 0, 0,'x')).toBeNull();
+    })
+
+    //checks if length of ship exceeds gameboard
+    //checks if there are any available spots for ship
+    //checks if ships can be placed next to each other?
+}) 
+
+describe('Gameboard: receiveAttack', () => {
+    it('tests whether the attack hits a ship or not', () => {
+        const gameboard = Gameboard();
+        gameboard.placeShip(Ship(3), 0, 0, 'x');
+        expect(gameboard.receiveAttack(0, 0)).toBe(true);
+        expect(gameboard.receiveAttack(1, 0)).toBe(true);
+        expect(gameboard.receiveAttack(2, 0)).toBe(true);
+        expect(gameboard.receiveAttack(0, 1)).toBe(false);
+        expect(gameboard.receiveAttack(0, 2)).toBe(false);
+        expect(gameboard.receiveAttack(0, 3)).toBe(false);
+    })
 })
